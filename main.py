e1b9bc48ac66afa33d30b9f014e0a5383f097796
terrrale <100957573+terrrale@users.noreply.github.com>
2024-08-21 21:34:08 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1st program
+
+print('1st program')
+print((9**0.5) * 5)
+
+# 2nd program
+print('2nd program')
+print(9.99 > 9.98 and 1000 != 1001)
+
+# 3rd program
+print('3rd program')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+print('4th program')
+example = '123.456'
+example = float(example) * 10
+print(int(example) % 10)
