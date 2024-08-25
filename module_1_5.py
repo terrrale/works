5721786461a6ae10f1a0e002e833a76d75385c5a
terrrale <100957573+terrrale@users.noreply.github.com>
2024-08-25 20:23:00 +0500

    Create module_1_5.py

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 15, 10, "Hello"
+print(immutable_var)
+#immutable_var[0] = 2
+# *Кортеж относится к неизменяемым объектам
+mutable_list = ["good", "work", 20, 4, 9]
+mutable_list[1] = "weekend"
+mutable_list[3] = 11
+print(mutable_list)
